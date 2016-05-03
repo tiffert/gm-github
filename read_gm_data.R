450f17985b38c9dfcacd4c89590c90c845bdf838
tiffert <tiffert@gmail.com>
2016-05-03 14:57:51 -0400

    Added 1957 data

--- read_gm_data.R
+++ read_gm_data.R
@@ -1,5 +1,11 @@
 gdp <- read.csv("data/data-1952.csv")
+new <- read.csv("data/data-1957.csv")
+gdp<-rbind(gdp,new)
 
 gdp$gdp<-gdp$gdpPercap*gdp$pop
+
+keep<- c('country','year','lifeExp','gdp')
+
+gdp <- gdp[keep]
 
 write.csv(gdp, "gdpc.csv")
